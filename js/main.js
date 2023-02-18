2c0cd4b4ea7acd3bb8b677065f4addf3e0de0eca
Serhii Kryvorotenko <serhii@MacBook-Pro-Serhii.local>
2023-02-18 17:14:52 +0100

    finish

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const a = +prompt('число А:');
+const b = Number(prompt('число Б:'));
+
+const sum = a + b;
+const subtraction = a - b;
+const multiplication = a * b;
+const division = a / b;
+
+console.log(a + ' + ' + b + ' = ' + sum);
+console.log(a + ' - ' + b + ' = ' + subtraction);
+console.log(a + ' * ' + b + ' = ' + multiplication);
+console.log(a + ' / ' + b + ' = ' + division);
